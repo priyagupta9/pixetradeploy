2fe3a5bd2fee2b5bc96f81fe0282d71d0d89787f
Harsh Kushwah <harshkingkushwah@gmail.com>
2022-04-04 15:37:18 +0530

    Home page v-3

--- src/components/ProductsPage/Product.jsx
+++ src/components/ProductsPage/Product.jsx
@@ -8,20 +8,9 @@ import { Link } from 'react-router-dom';
 
 const data=[1,2,3,4]
 function Product() {
-<<<<<<< HEAD
-  const {state} = useLocation();
-  const name = state.name;
-  if(name=== null){
-    name = "User";
-  }
-  return (
-    <div className='product-page'>
-      <Header name={name} />
-=======
   return (
     <div className='product-page'>
       <Header name= "" />
->>>>>>> 1c3d4889a3f7a62ce4c701f2326faedd74478397
       {
         data.map(x=>(
           <Link to="/product-name" ><Photobook img={photobookbg} /></Link>
--- src/components/Sliders/CollectionSlider.js
+++ src/components/Sliders/CollectionSlider.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-// import lala from "./images/512.png"
+
 // css 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
